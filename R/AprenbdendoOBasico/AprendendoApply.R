2b0214dc265dcf158bb07310d5f18f37ec7822f3
Vinícius Caetano <viny6888@gmail.com>
2022-01-10 19:37:19 -0300

    Adicionar README e organizar as pastas

---
+++ R/AprenbdendoOBasico/AprendendoApply.R
@@ -0,0 +1,81 @@
+  # Família Apply
+
+# apply() - arrays e matrizes
+# tapply() - Os vetores podem ser divididos em diferentes subsets
+# lapply() - vetores e listas
+# sapply() - versão amigável da lapply
+# vapply() - similar a sapply, com valor de retorno modificado
+# rapply() - simpliar a lapply()
+# eapply() - gera uma lista
+# maplly() - similar a sapply, multivariada
+# by
+
+
+# list, numeric, character (list/vetor) => sapply ou lapply
+#matrix, data.frame (agregação por coluna) => by / tapply
+# Operações por linha ou operações específicas => apply
+
+#Usando um Loop
+lista1 <- list(a = (1:10), b = (45:77))
+
+#Calculando o valor total de cada elemento usando for
+valor_a <- 0
+valor_b <- 0
+
+for (i in lista1$a){
+    valor_a <- valor_a + i
+}
+for (j in lista1$b){
+    valor_b <- valor_b + j
+}
+print(valor_a)
+print(valor_b)
+
+
+#Usando sapply
+?sapply
+sapply(lista1, sum)
+sapply(lista1, mean)
+
+# apply()
+?apply
+
+x <- matrix(rnorm(9), nr = 3, byrow =T)
+x
+
+apply(x, 1, mean)
+apply(x, 2, mean)
+apply(x, 1, plot)
+
+
+resultapply <- apply(x, 1, mean)
+
+
+escola <- data.frame(Aluno = c('Alan', 'Alice', 'Alana', 'Aline', 'Alex', 'Ajay'),
+                     Matematica = c(90, 80, 85, 87, 56, 79),
+                     Geografia = c(100, 78, 86, 90, 98, 67),
+                     Quimica = c(76, 56, 89, 90, 100, 87))
+
+escola
+
+#$Calculando a Média
+
+escola$Media = NA
+
+escola$Media = apply(escola[,c(2, 3, 4)], 1, mean)
+escola$Media = round(escola$Media)
+
+# tapply() vs sqldf
+install.packages('sqldf')
+require(sqldf)
+
+escola2 <- data.frame(Aluno = c('Alan', 'Alice', 'Alana', 'Alan', 'Alice', 'Alana'),
+                     Smestre = c(1 ,1 ,1 ,2 , 2, 2),
+                     Matematica = c(90, 80, 85, 87, 56, 79),
+                     Geografia = c(100, 78, 86, 90, 98, 67),
+                     Quimica = c(76, 56, 89, 90, 100, 87))
+
+escola2
+sqldf("select Aluno, sum(Matematica), sum(Geografia), sum(Quimica) from escola2 group by Aluno")
+
+by(escola2[, c(2, 3, 4)])
---
+++ README.md
@@ -0,0 +1,77 @@
+# Estudos Diversos
+
+
+
+## Objetivo
+
+Reunir seções de estudos sobre tecnologias diversas.
+
+
+
+## Tecnologias estudadas atualmente
+
+- **Python**
+
+  Conceitos estudados:
+
+  - POO
+
+    > Estudos sobre como trabalhar com objetos em Python.
+
+  - Testes automatizados
+
+    > Como fazer testes de software utilizando a biblioteca unittest e assim, trabalhar
+    >
+    > com TDD (Test-Driven Development).
+
+  - Design Patterns
+
+    > Descobrir e estudar padrões de projetos utilizados para resolver os mais dversos
+    >
+    > problemas que frequentemente ocorrem no contexto da programação.
+
+  - PyAutoGui
+
+    > Utilizar uma biblioteca de automação de GUI que serve para mover, selecionar e utilizar os comandos
+    >
+    > de teclado e mouse sem intervenção humana.
+
+  - Python Eficaz
+
+    > Sessão de estudo do livro ["Python Eficaz"]([Python Eficaz: 59 Maneiras de Programar Melhor em Python | Amazon.com.br](https://www.amazon.com.br/Python-Eficaz-Maneiras-Programar-Melhor/dp/8575225103)) que ensina a como melhorar e utilizar melhor 
+    >
+    > as funções embutidas no Python.
+
+  - Tkinter
+
+    > Sessões de estudo baseadas na construção de interfaces gráficas em Python.
+
+
+
+- **R**
+
+  Conceitos estudados:
+
+  - Básico
+
+    > Sessões de estudo baseadas em entender e compreender o funcionamento da linguagem R, como sua sintaxe, suas estruturas de arnazenamento de dados, funções built-in, entre outros.
+
+
+
+- Java
+
+  Conceitos Estudados	
+
+  - Estruturas de Controle
+  - Métodos
+  - Armazenamento de dados
+
+
+
+- Bootstrap
+  - Básico
+
+
+
+
+
