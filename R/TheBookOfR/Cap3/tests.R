5e90610e7f2e0b1f6871debd9a0b304042ffe356
Vinícius Caetano <viny6888@gmail.com>
2022-01-28 08:17:34 -0300

    Exercício 3.3 adcionado

---
+++ R/TheBookOfR/Cap3/tests.R
@@ -0,0 +1,9 @@
+A <- matrix(data=c(-3,2,893,0.17), nrow=2, ncol=2)
+matrix(data=c(1,2,3,4,5,6),nrow=2,ncol=3,byrow=T)
+
+mat <-  rbind(c(1,3,4), 5:3, c(100, 20, 90), 11:13)
+
+
+A <- rbind(c(2,5,2),c(6,1,4))
+B <- cbind(c(3,-1,1),c(-3,1,5))
+A%*%B
