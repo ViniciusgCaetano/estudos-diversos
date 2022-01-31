41d5aeed402a215cc2642bd790879778a569f2b6
Vinícius Caetano <viny6888@gmail.com>
2022-01-31 11:54:11 -0300

    Adicionados exercícios do cap 4

--- R/TheBookOfR/Cap3/tests.R
+++ R/TheBookOfR/Cap3/tests.R
@@ -7,3 +7,6 @@ mat <-  rbind(c(1,3,4), 5:3, c(100, 20, 90), 11:13)
 A <- rbind(c(2,5,2),c(6,1,4))
 B <- cbind(c(3,-1,1),c(-3,1,5))
 A%*%B
+
+AR <-  array(data=1:24, dim=c(3,4,2))
+BR <- array(data=rep(1:24,times=3),dim=c(3,4,2,3))
